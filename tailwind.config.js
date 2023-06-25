b799440a3ec7b11007c894613d39f6e268b22e20
ansarullahsadat <ansarullahsadat@gmail.com>
2023-06-25 19:42:03 +0430

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      sans: ["Roboto Mono"],
+      body: ["Roboto Mono"],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
